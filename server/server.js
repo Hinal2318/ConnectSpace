3e149425b9d9d3dae9d539dca5f6b4e3198ebfdd
Hinal Patel <hinal231105@gmail.com>
2026-02-21 16:16:38 +0530

    Added backend auth system

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.js"
+dotenv.config();
+
+const app=express()
+
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log("MONGODB connected"))
+.catch(err=> console.log(err))
+
+app.use("/api/auth",authRoutes);
+app.listen(5000,()=>console.log("server is running on port 5000"))
